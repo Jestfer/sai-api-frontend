d61ed9dff2a9d4cd119f8dcfa2306e044cfcdd0b
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-28 13:43:04 +0100

    added FormGroup and dialogRef (to pass Dialog info to parent on save/close) into CourseDialogComponent

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '../../../node_modules/@angular/forms';
+import { MatDialogRef } from '../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-course-dialog',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDialogComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>
+  ) { }
 
   ngOnInit() {
   }
 
+  save() {
+    this.dialogRef.close(this.form.value);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 }
