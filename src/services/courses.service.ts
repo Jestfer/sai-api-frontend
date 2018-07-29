5cb2cfafb8fe265dce6f2c694a2e782fd481e016
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-29 10:29:56 +0100

    added method in service to post CourseDialog

--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CourseDialogComponent } from '../app/course-dialog/course-dialog.component';
+import { Observable } from 'rxjs';
+
+// TODO: change this and make it autoincrement in Spring Boot
+// let id: Number = 0;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   addCourse(course: CourseDialogComponent): Observable<CourseDialogComponent> {
+     // Id needs to be handled
+     return this.http.post<CourseDialogComponent>('/topics/1/courses', course);
+   }
 }
