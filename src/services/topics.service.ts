8a841f5a7489b28960c77ae3b0f291a454bd2b8c
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-30 20:16:18 +0100

    IMPLEMENTATION with BehaviourSubject

--- src/services/topics.service.ts
+++ src/services/topics.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject, of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TopicsService {
   topics = [];
-  // observable único, q lo mandamos en el else... q ya tiene los datos de la llamada del If
+  // tipo de observable único, ... q ya tiene los datos de la llamada del If
+  // Y este obs lo manejamos en los nuevos métodos del addTopic (con el refresh y demás)
   private obs = new BehaviorSubject<any>(this.topics);
 
   // provider Http, hacemos llamadas
   constructor(private http: HttpClient) {}
 
-  getTopics(): Observable<any> {
-    // Cómo manejar los Observers, "of", para usarlo solo una vez
-
-    // Construimos el elemento para emitir cosas
-    // Q admite modificaciones... para topics
+  // Emitimos el BehaviorSubject
+  getTopics(): BehaviorSubject<any> {
+    // Construimos el elemento para emitir cosas - Q admite modificaciones... para topics
     if (!this.topics.length) {
       // get siempre devuelve un Observer
       this.http.get('http://localhost:8089/topics')
@@ -29,22 +28,18 @@ export class TopicsService {
             this.obs.next(this.topics);
           }
         );
-    } else {
+    }
       // Decirle al componente q como hay topics, mandamos el siguiente y que reaccione
 
       // PROGRAMACIÓN BASADA EN ESTADOS
       // Los servicios son los que se encargan del movimiento y transformación de datos
       // Los componentes reaccionan para pintar
 
-      // Si metemos un nuevo observable aquí, el servicio no tiene el control sobre
-      // el observado... hay que intentar trabajar solo con un observable
-      return of(this.obs);
-      // return new Observable((observ) => observ.next(this.topics));
-    }
-
-    // Se transforma en Observable para emitirlo y q el componente lo maneje con el subscribe
-    return this.obs.asObservable();
-    // EL COMPONENTE SE SUSCRIBE a  "obs" hasta que tenga algo..., es decir, hasta que cambie... porque estamos suscritos
-    // Esta función devuelve un observable al que el componente se suscribe, y hasta que no cambie, estaremos suscritos
+      // NEW: devolvemos un Behaviour Subject
+      // Que es como el estado, que está por debajo del observable
+      // Y todos se suscriben
+    return this.obs;
+    // EL COMPONENTE SE SUSCRIBE a "obs" hasta que tenga algo..., es decir, hasta que cambie...
+    // Cuando cambie su estado, todos se repintan y sus valores cambian
   }
 }
