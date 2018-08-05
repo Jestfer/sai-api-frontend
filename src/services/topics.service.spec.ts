9c9660a1e67e10b8786feda8a3093ddf527b3e55
Josué Estévez Fernández <jestfer6@gmail.com>
2018-08-05 12:14:10 +0100

    testing HttpClient get req with HttpTestingController, next: refactor the way of inject providers into beforeEach with TestBed.get(providerName), angular.io

--- src/services/topics.service.spec.ts
+++ src/services/topics.service.spec.ts
@@ -1,44 +1,97 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import { ConnectionBackend, ResponseOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+// https://angular.io/guide/http#testing-http-requests
+// Stackblitz example: https://stackblitz.com/angular/eaajdybomrjm
+// The below lib makes HTTP backend mocking straightforward
+// Pattern:
+// 1- Req is made first
+// 2- Then a test expects that certain reqs have or not been made and perform assertions against such reqs
+// 3- Provides response by "flushing" each expected req
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TopicsService } from './topics.service';
+import { Data } from '@angular/router';
+
+const testUrl = '/data';
 
 describe('TopicsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        TopicsService, HttpClient, HttpHandler,
-        { provide: ConnectionBackend, useClass: MockBackend }
-       ]
+        TopicsService,
+        HttpClient,
+        HttpHandler,
+        HttpClientTestingModule,
+        HttpTestingController
+      ],
+      imports: [ HttpClientTestingModule ]
     });
   });
 
-  describe('#getTopics()', () => {
-    it('should be created', inject([TopicsService], (service: TopicsService) => {
-      expect(service).toBeTruthy();
-    }));
+  it('can test HttpClient get req', inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+    const testData: Data = { name: 'Test Data'};
 
-    it('should return a BehaviourSubject<any<Topic>>',
-      inject([TopicsService, ConnectionBackend], (service: TopicsService, mockBackend) => {
+    // Make an HTTP GET req
+    http.get<Data>(testUrl)
+    .subscribe(data =>
+      // When observable resolves, result should match testData
+      expect(data).toEqual(testData)
+    );
 
-      const mockResponse = {
-          name: 'Chess',
-          decription: 'Best chess courses'
-      };
+    // expectOne() will match req's URL, if not, expectOne() would throw
+    const req = backend.expectOne('/data');
 
-      mockBackend.connections.subscribe((connection) => {
-        connection.mockRespond(new Response(new ResponseOptions({
-          body: JSON.stringify(mockResponse)
-        })));
-      });
+    // Assert that req is a GET
+    expect(req.request.method).toEqual('GET');
 
-      service.getTopics().subscribe((topicData) => {
-        expect(topicData.length).toBe(1);
-        expect(topicData.name).toEqual('Chess');
-        expect(topicData.description).toEqual('Best chess courses');
-      });
-    }));
+    // Respond with mock data, causing Observable to resolve
+    // Subscribe callback asserts that correct data was returned
+    req.flush(testData);
+
+    // Finally, we assert that there are no outstanding reqs
+    backend.verify();
+  }));
+
+  xit('should issue a request',
+    // 1. declare as async test since the HttpClient works with Observables
+    async(
+      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+        // 3. send a simple request
+        http.get('/foo/bar').subscribe();
+
+        // 4. HttpTestingController supersedes `MockBackend` from the "old" Http package
+        // here two, it's significantly less boilerplate code needed to verify an expected request
+        backend.expectOne({
+          url: '/foo/bar',
+          method: 'GET'
+        });
+      })
+    ));
   });
-});
+  // describe('#getTopics()', () => {
+  //   it('should be created', inject([TopicsService], (service: TopicsService) => {
+  //     expect(service).toBeTruthy();
+  //   }));
+
+    // it('should return a BehaviourSubject<any<Topic>>',
+    //   inject([TopicsService, ConnectionBackend], (service: TopicsService, mockBackend) => {
+
+    //   const mockResponse = {
+    //       name: 'Chess',
+    //       decription: 'Best chess courses'
+    //   };
+
+    //   mockBackend.connections.subscribe((connection) => {
+    //     connection.mockRespond(new Response(new ResponseOptions({
+    //       body: JSON.stringify(mockResponse)
+    //     })));
+    //   });
+
+    //   service.getTopics().subscribe((topicData) => {
+    //     expect(topicData.length).toBe(1);
+    //     expect(topicData.name).toEqual('Chess');
+    //     expect(topicData.description).toEqual('Best chess courses');
+    //   });
+    // }));
+  // });
+// });
