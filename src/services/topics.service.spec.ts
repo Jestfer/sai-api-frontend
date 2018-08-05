7170e4334419c006eb48f35d4b41dcd071529668
Josué Estévez Fernández <jestfer6@gmail.com>
2018-08-05 12:27:38 +0100

    Http get req concept tests are now working, seems provider issues have been fixed

--- src/services/topics.service.spec.ts
+++ src/services/topics.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject, async } from '@angular/core/testing';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 // https://angular.io/guide/http#testing-http-requests
 // Stackblitz example: https://stackblitz.com/angular/eaajdybomrjm
 // The below lib makes HTTP backend mocking straightforward
@@ -13,33 +13,44 @@ import { TopicsService } from './topics.service';
 import { Data } from '@angular/router';
 
 const testUrl = '/data';
+let httpClient;
+let httpTestingController;
 
 describe('TopicsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        TopicsService,
-        HttpClient,
-        HttpHandler,
-        HttpClientTestingModule,
-        HttpTestingController
+        TopicsService
       ],
-      imports: [ HttpClientTestingModule ]
+      imports: [HttpClientTestingModule]
     });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('can test HttpClient get req', inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
-    const testData: Data = { name: 'Test Data'};
+  afterEach(() => {
+    // Finally, we assert that there are no outstanding reqs in each test
+    httpTestingController.verify();
+  });
+
+  it('should be created', inject([TopicsService], (service: TopicsService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('can test HttpClient get req', () => {
+    const testData: Data = { name: 'Test Data' };
 
     // Make an HTTP GET req
-    http.get<Data>(testUrl)
-    .subscribe(data =>
-      // When observable resolves, result should match testData
-      expect(data).toEqual(testData)
-    );
+    httpClient.get<Data>(testUrl)
+      .subscribe(data =>
+        // When observable resolves, result should match testData
+        expect(data).toEqual(testData)
+      );
 
     // expectOne() will match req's URL, if not, expectOne() would throw
-    const req = backend.expectOne('/data');
+    const req = httpTestingController.expectOne('/data');
 
     // Assert that req is a GET
     expect(req.request.method).toEqual('GET');
@@ -47,32 +58,30 @@ describe('TopicsService', () => {
     // Respond with mock data, causing Observable to resolve
     // Subscribe callback asserts that correct data was returned
     req.flush(testData);
-
-    // Finally, we assert that there are no outstanding reqs
-    backend.verify();
-  }));
-
-  xit('should issue a request',
-    // 1. declare as async test since the HttpClient works with Observables
-    async(
-      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
-        // 3. send a simple request
-        http.get('/foo/bar').subscribe();
-
-        // 4. HttpTestingController supersedes `MockBackend` from the "old" Http package
-        // here two, it's significantly less boilerplate code needed to verify an expected request
-        backend.expectOne({
-          url: '/foo/bar',
-          method: 'GET'
-        });
-      })
-    ));
   });
-  // describe('#getTopics()', () => {
-  //   it('should be created', inject([TopicsService], (service: TopicsService) => {
-  //     expect(service).toBeTruthy();
-  //   }));
 
+  // 1. declare as async test since the HttpClient works with Observables ** NOT NEEDED REALLY
+  it('should issue a request', async(() => {
+    // 3. send a simple request
+    httpClient.get('/foo/bar').subscribe();
+
+    // 4. HttpTestingController supersedes `MockBackend` from the "old" Http package
+    // here two, it's significantly less boilerplate code needed to verify an expected request
+    httpTestingController.expectOne({
+      url: '/foo/bar',
+      method: 'GET'
+    });
+  }));
+});
+
+
+
+
+
+
+
+
+  // describe('#getTopics()', () => {
     // it('should return a BehaviourSubject<any<Topic>>',
     //   inject([TopicsService, ConnectionBackend], (service: TopicsService, mockBackend) => {
 
